bcaf27d76e62637ea44becfd4017c46953f449e6
Omar <omar2535@gmail.com>
2018-02-04 18:15:54 -0800

    fixed untracked

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-//Add to .gitignore
-
-module.exports = {
-    google: {
-        clientID: "928301875641-2fq2p5gcpk92frmp14k2bd6oa30tq23u.apps.googleusercontent.com",
-        clientSecret: "jPQ7Rslu0nROyFuPG7IeG29i",
-    }
-};
