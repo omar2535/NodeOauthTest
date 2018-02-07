d66ec5a494d7d5f643b136bdcb192cecaa0339e0
Omar <omar2535@gmail.com>
2018-02-06 23:16:22 -0800

    Added cookie serializer & deserializer. Changed DB to mlab instead of localDB

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const app = express();
 const authRoutes = require('./routes/oauth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 //set up view
 
 app.set("view engine", 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/Oauth", (error, db)=>{
-    if(error){
-        return console.log("Unable to connect to database server");
-    }
-    console.log("Connected!");
-    db.close();
+mongoose.connect(keys.mongodb.mongodbURI, ()=>{
+    console.log("connected");
 });
 
 
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-models');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
     //Options for google strat
@@ -11,12 +21,25 @@ passport.use(
     callbackURL:"/auth/google/redirect",
     
 }, (accessToken, refreshToken, profile, done)=>{
-    new User({
-        username: profile.displayName,
-        googleId: profile.id,
-    }).save().then((newUser)=>{
-        console.log("added new user" + newUser);
+    //Check if user already exists in DB
+    User.findOne({
+        googleId: profile.id
+    }).then((currentUser)=>{
+        if(currentUser){
+            //Already have user
+            console.log("user already exists");
+            done(null, currentUser);
+        }else{
+            //if not, create user in DB
+            new User({
+                username: profile.displayName,
+                googleId: profile.id,
+            }).save().then((newUser)=>{
+                done(null, newUser);
+            });
+        }
     });
+
     })
 );
 
